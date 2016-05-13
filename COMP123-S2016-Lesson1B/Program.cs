38207a7b9f27d6e88db8e29e603f389d9e293127
jimmymcperson <undeadwalrus@hotmail.com>
2016-05-13 10:43:53 -0400

    Initial Commit

---
+++ COMP123-S2016-Lesson1B/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * Author: Jonathan Hao
+ * Date Created: May 13, 2016
+ * Date Modified: May 13, 2016
+ * Student ID: 300869273
+ * 
+ * Description: This program demonstrates GitHub and version control
+ * best practices.  We also expore Unit Testing.
+ * 
+ * Version: 0.1 - Initial Version
+ */
+
+
+namespace COMP123_S2016_Lesson1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
